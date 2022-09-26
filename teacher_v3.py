d111ed866074e0fb2ea4897446d2730a734facc0
Puning97 <114408373+Puning97@users.noreply.github.com>
2022-09-26 21:40:57 +0800

    Update teacher_v3.py

--- teacher_v3.py
+++ teacher_v3.py
@@ -3,12 +3,12 @@ from feature_transformer_CCL import Feature_Trans_img as create_model
 from collections import OrderedDict
 def id_teacher():
     checkpoint = torch.load('/data/chpt/ACCV2022/sota/weights/idteacher_66.13_1_6_1024.pth', map_location='cpu')
-    #checkpoint = torch.load('C:/Users/Puning.Yang/Desktop/sota/id_head6_drop0_65.4.pth', map_location='cpu')
+    
     new_state_dict = OrderedDict()
     for k, v in checkpoint.items():
         k_new = k[7:]
         new_state_dict[k_new] = v
-    #model = create_model(pretrained=True)
+    
     checkpoints = {k: v for k, v in new_state_dict.items()}
     model=create_model()
     model_dict = model.state_dict()
@@ -21,7 +21,7 @@ def moion_teacher():
     for k, v in checkpoint.items():
         k_new = k[7:]
         new_state_dict[k_new] = v
-    #model = create_model(pretrained=True)
+    
     checkpoints = {k: v for k, v in new_state_dict.items()}
     model=create_model()
     model_dict = model.state_dict()
